a8f5ecd9b643629b1db87f3d7f1dc1b0fef111ca
hapiz2022 <112926419+hapiz2022@users.noreply.github.com>
2022-09-19 07:59:36 +0700

    Create menghitung_harga_total.cpp

---
+++ menghitung_harga_total.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define harga 4500.02 //…menentukan harga dengan definevoid main()
+
+int main(){  
+    float jumlah,total;
+    
+    cout << "Masukan jumlah barang = ";
+    cin  >> jumlah; total=harga*jumlah;
+
+    cout << "\nYang harus di bayar = " << total;
+    return 0;
+}
